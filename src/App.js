a0e4f48de9c85ad016b5dc41d8a8d27458e6d715
Igor Brandalise <brandaliseigor@gmail.com>
2024-10-30 21:26:51 -0300

    upd style

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
-import { FiSearch } from 'react-icons/fi';
-import './style.css' 
+import { FiSearch } from "react-icons/fi";
+import "./style.css";
 
 function App() {
   return (
     <div className="container">
       <h1 className="title">Buscador de CEP</h1>
 
-      <div className="container">
+      <div className="containerInput">
         <input type="text" placeholder="Digite seu CEP..."></input>
 
         <button className="button"></button>
-        < FiSearch size={25} color="#fff"/>
+        <FiSearch size={25} color="#fff" />
       </div>
 
-    <main className='main'>
-      <h2>CEP: 79003222</h2>
+      <main className="main">
+        <h2>CEP:</h2>
 
-      <span>Reua</span>
-      <span>Complemento:</span>
-      <span>Vila Rosa</span>
-      <span>Campo Grande - MS</span>
-    </main>
-
+        <span>Rua:</span>
+        <span>Complemento:</span>
+        <span>Bairro:</span>
+        <span>Localização:</span>
+      </main>
     </div>
   );
 }
